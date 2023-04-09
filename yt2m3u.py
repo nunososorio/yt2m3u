1797cc373f526ac954b62f3a90cb0913308152a6
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-09 19:46:04 +0100

    nn

--- yt2m3u.py
+++ yt2m3u.py
@@ -7,9 +7,8 @@ playlist_url = st.text_input('Enter YouTube playlist URL:')
 
 if playlist_url:
     playlist = Playlist(playlist_url)
-    m3u_file = open('playlist.m3u', 'w')
+    m3u_content = ''
     for video in playlist.videos:
         video_url = video.streams.filter(progressive=True).first().url
-        m3u_file.write(video_url + '\n')
-    m3u_file.close()
-    st.download_button(label='Download m3u file', data='playlist.m3u', mime='audio/x-mpegurl')
+        m3u_content += video_url + '\n'
+    st.download_button(label='Download m3u file', data=m3u_content, mime='audio/x-mpegurl', file_name='playlist.m3u')
