e7e31b64cad100f8335c4ccd839dd34378b8b49f
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-09 22:43:19 +0100

    Atualizar o yt2m3u.py

--- yt2m3u.py
+++ yt2m3u.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from pytube import Playlist
+import re
+from datetime import datetime
 
 st.title('yt2m3u - YouTube Playlist to m3u Downloader')
 
@@ -10,11 +12,11 @@ playlist_url = st.text_input('Enter YouTube playlist URL:', value='https://m.you
 if playlist_url:
     playlist_history.append(playlist_url)
     st.session_state['playlist_history'] = playlist_history[-5:]
-    
+
     st.write('Playlist history:')
     for url in playlist_history:
         st.write(url)
-    
+
     playlist = Playlist(playlist_url)
     m3u_content = '#EXTM3U\n'
     for video in playlist.videos:
@@ -22,3 +24,20 @@ if playlist_url:
         video_url = video_stream.url
         m3u_content += f'#EXTINF:-1,{video.title}\n{video_url}\n'
     st.download_button(label='Download m3u file', data=m3u_content, mime='audio/x-mpegurl', file_name='playlist.m3u')
+
+    # Open and read the playlist.m3u file
+    with open('playlist.m3u', 'r') as f:
+        m3u_content = f.read()
+
+    # Extract the sparams value using a regular expression
+    sparams_match = re.search(r'sparams=([^&]+)', m3u_content)
+    if sparams_match:
+        sparams = sparams_match.group(1)
+        # Parse the sparams value to extract the expire parameter
+        expire_match = re.search(r'expire%3D(\d+)', sparams)
+        if expire_match:
+            expire = int(expire_match.group(1))
+            # Convert the expire parameter to a datetime object
+            expire_datetime = datetime.fromtimestamp(expire)
+            # Display the expiration datetime in the app
+            st.write(f'Download will expire on: {expire_datetime}')
