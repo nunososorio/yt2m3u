f6f3895acc8fbdbefcf2f9458a0727ad75a934f6
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-09 23:14:50 +0100

    Atualizar o yt2m3u.py

--- yt2m3u.py
+++ yt2m3u.py
@@ -1,39 +1,21 @@
 import streamlit as st
 from pytube import Playlist
-import re
-from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor
 
 st.title('yt2m3u - YouTube Playlist to m3u Downloader')
-
-playlist_history = st.session_state.get('playlist_history', [])
 
 playlist_url = st.text_input('Enter YouTube playlist URL:', value='https://m.youtube.com/playlist?list=PL-gWsve6MojDIwDIhpxhMlqEn3dKz8f9a')
 
 if playlist_url:
-    playlist_history.append(playlist_url)
-    st.session_state['playlist_history'] = playlist_history[-5:]
-
-    st.write('Playlist history:')
-    for url in playlist_history:
-        st.write(url)
-
     playlist = Playlist(playlist_url)
     m3u_content = '#EXTM3U\n'
-    for video in playlist.videos:
+
+    def get_video_stream(video):
         video_stream = video.streams.filter(progressive=True).order_by('resolution').desc().first()
         video_url = video_stream.url
-        m3u_content += f'#EXTINF:-1,{video.title}\n{video_url}\n'
-    st.download_button(label='Download m3u file', data=m3u_content, mime='audio/x-mpegurl', file_name='playlist.m3u')
+        return f'#EXTINF:-1,{video.title}\n{video_url}\n'
 
-    # Extract the sparams value using a regular expression
-    sparams_match = re.search(r'sparams=([^&]+)', m3u_content)
-    if sparams_match:
-        sparams = sparams_match.group(1)
-        # Parse the sparams value to extract the expire parameter
-        expire_match = re.search(r'expire%253D(\d+)', sparams)
-        if expire_match:
-            expire = int(expire_match.group(1))
-            # Convert the expire parameter to a datetime object
-            expire_datetime = datetime.fromtimestamp(expire)
-            # Display the expiration datetime in the app
-            st.write(f'Download will expire on: {expire_datetime}')
+    with ThreadPoolExecutor() as executor:
+        m3u_content += ''.join(executor.map(get_video_stream, playlist.videos))
+
+    st.download_button(label='Download m3u file', data=m3u_content, mime='audio/x-mpegurl', file_name='playlist.m3u')
