bbde652e74ff41c53cc65186260ca281e2a4079a
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-09 19:12:05 +0100

    msd

--- yt2m3u.py
+++ yt2m3u.py
@@ -1,6 +1,5 @@
-import concurrent.futures
 import streamlit as st
-from pytube import Playlist, YouTube
+from pytube import Playlist
 
 st.title("yt2m3u - YouTube Playlist to m3u")
 
@@ -10,17 +9,16 @@ channel_url = st.text_input("Enter the YouTube playlist URL:", value="https://m.
 if channel_url:
     # Create a Playlist object
     playlistx = Playlist(channel_url)
-    # Create a list of video objects from the playlist URLs
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        videos = list(executor.map(YouTube, playlistx))
     # Create a list of video download URLs
-    video_urls = [video.streams.get_highest_resolution().url for video in videos]
-    # Clear the contents of the dytchan2.m3u file
-    open("dytchan2.m3u", "w").close()
-    # Write the video download URLs to the dytchan2.m3u file
-    with open("dytchan2.m3u", "w") as f:
+    video_urls = [video.streams.get_highest_resolution().url for video in playlistx]
+    # Write the video download URLs to a local m3u playlist file named dytchan2.m3u
+    with open("pages/dytchan2.m3u", "w") as f:
         f.write("#EXTM3U\n")
         for url in video_urls:
             f.write(f"#EXTINF:-1,{url}\n{url}\n")
-    st.success("Playlist downloaded successfully!")
-    st.markdown("Download the m3u file [here](dytchan2.m3u)")
+    st.success("Playlist conversion was successfull!")
+    
+    # Create a download button for the m3u file
+    with open("pages/dytchan2.m3u", "r") as f:
+        file_contents = f.read()
+        st.download_button(label="Download m3u file", data=file_contents, file_name="dytchan2.m3u", mime="audio/x-mpegurl")
