7fae30ef0eb80d8aeda4ca8f4af1c27114dc2ca9
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-23 00:22:10 +0100

    Update localapp.py

--- localapp.py
+++ localapp.py
@@ -1,60 +1,63 @@
- 
 import streamlit as st
+
 from pytube import Playlist
+
 from concurrent.futures import ThreadPoolExecutor
 
 st.title("yt2m3u - YouTube Playlist to m3u Downloader")
 
 # Get the playlist URL from the user
-playlist_url = st.text_input("Enter YouTube playlist URL:", value="https://m.youtube.com/playlist?list=PL-gWsve6MojDIwDIhpxhMlqEn3dKz8f9a")
+
+playlist_url = st.text_input("Enter YouTube playlist URL:", value="https://m.youtube.com/playlist?list=PL-gWsve6MojDIwDIhpxhMlqEn3dKz8f9a") 
 
 if playlist_url:
-    playlist = Playlist(playlist_url)
-    m3u_content = "#EXTM3U\n"
 
-    # Define a function to get the video stream for a given video
-    def get_video_stream(video):
-        # Try to get the video stream, and handle any errors that might occur
-        try:
+    try: 
+
+        playlist = Playlist(playlist_url)
+
+    except KeyError:
+
+        st.error("Invalid playlist URL")
+
+# Write m3u file to disk    
+
+with open("ytplay.m3u", "w") as f:
+
+    f.write("#EXTM3U\n")
+
+progress_bar = st.progress(0)  
+
+with ThreadPoolExecutor() as executor:
+
+    for i, video in enumerate(playlist.videos):     
+
+        # Update progress bar
+
+        progress_bar.progress((i+1)/len(playlist.videos))
+
+        
+
+        # Get video stream 
+
+        try:  
+
             video_stream = video.streams.filter(progressive=True).order_by("resolution").desc().first()
+
             video_url = video_stream.url
-            video_title = video.title
-            video_default_filename = video.default_filename
-        except KeyError:
-            st.warning(f"No streaming data available for video: {video.title}")
-            video_url = None
-            video_title = video.title
-            video_default_filename = None
-        except Exception as e:
-            st.error(f"Error processing video {video.title}: {e}")
-            video_url = None
-            video_title = video.title
-            video_default_filename = None
 
-        # Return the video stream as an m3u entry
-        if video_url:
-            if video_default_filename:
-                return f"#EXTINF:-1,{video_default_filename}\n{video_url}\n"
-            else:
-                return f"#EXTINF:-1,{video_title}\n{video_url}\n"
-        else:
-            return ""
+            with open("ytplay.m3u", "a") as f:
 
-    # Use a thread pool to download video streams concurrently
-    with ThreadPoolExecutor() as executor:
-        # Try to get the video stream for each video in the playlist, and handle any errors that might occur
-        try:
-            m3u_content += "".join(executor.map(get_video_stream, playlist.videos))
-        except Exception as e:
-            st.error(f"Error processing playlist: {e}")
+                f.write(f"#EXTINF:-1,{video.title}\n{video_url}\n")
 
-    # Clean the content of the existing file
-    with open('ytplay.m3u', 'r') as f:
-        content = f.read().strip()
+        except KeyError:  
 
-    # Replace the content of the file with the new playlist
-    with open('ytplay.m3u', 'w') as f:
-        f.write(m3u_content)
+            st.warning(f"No streaming data available for video: {video.title}") 
 
-    # Display a success message to the user
-    st.success("Playlist successfully downloaded!")
+        except yt.exceptions.VideoPrivate:
+
+            st.warning(f"Video {video.title} is private")
+
+        except yt.exceptions.VideoDeleted:
+
+            st.warning(f"Video {video.title} has been deleted")
