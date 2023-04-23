ee202155608389d01ea61c4bf58c1343404aecc2
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-23 22:39:51 +0100

    Update yt2m3u.py

--- yt2m3u.py
+++ yt2m3u.py
@@ -1,32 +1,46 @@
 import streamlit as st
-from pytube import Playlist
-from concurrent.futures import ThreadPoolExecutor
+import pytube
 
-st.title('yt2m3u - YouTube Playlist to m3u Downloader')
+# Function to extract video links and titles using pytube
+def get_video_info(video_url):
+    try:
+        video = pytube.YouTube(video_url)
+        video_title = video.title
+        video_streams = video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
+        if video_streams:
+            video_url = video_streams[0].url
+        else:
+            video_url = None
+        return video_title, video_url
+    except Exception as e:
+        st.warning(f"Error extracting video information for {video_url}: {e}")
+        return None, None
 
-playlist_url = st.text_input('Enter YouTube playlist URL:', value='https://m.youtube.com/playlist?list=PL-gWsve6MojDIwDIhpxhMlqEn3dKz8f9a')
+# Main function
+def main():
+    st.title("YouTube Playlist Downloader")
+    playlist_url = st.text_input("Enter YouTube playlist URL:")
+    if not playlist_url:
+        st.warning("Please enter a playlist URL.")
+        return
 
-if playlist_url:
-    playlist = Playlist(playlist_url)
-    m3u_content = '#EXTM3U\n'
+    st.write("Extracting video information...")
+    playlist = pytube.Playlist(playlist_url)
+    with st.spinner(f"Extracting {len(playlist.video_urls)} video URLs..."):
+        video_data = [get_video_info(url) for url in playlist.video_urls]
 
-    def get_video_stream(video):
-        video_stream = video.streams.filter(progressive=True).order_by('resolution').desc().first()
-        video_url = video_stream.url
-        return f'#EXTINF:-1,{video.title}\n{video_url}\n'
+    download_links = []
+    for video_title, video_url in video_data:
+        if video_title and video_url:
+            # Format the download link with the #EXTINF tag
+            download_link = f"#EXTINF:-1,{video_title}\n{video_url}"
+            download_links.append(download_link)
+            st.write(f"- {video_title}")
 
-    with ThreadPoolExecutor() as executor:
-        m3u_content += ''.join(executor.map(get_video_stream, playlist.videos))
+    st.write("\n")
+    st.write("To download the videos, copy the following links and paste them into a downloader that supports M3U playlists:")
+    m3u_links = "#EXTM3U\n" + "\n".join(download_links)
+    st.code(m3u_links, language='text')
 
-    st.download_button(label='Download m3u file', data=m3u_content, mime='audio/x-mpegurl', file_name='playlist.m3u')
-
-st.write('## Potential Use Cases')
-
-st.write('The `yt2m3u` app can be useful in a variety of situations. For example, you could use it to:')
-st.write('- Download an m3u file for a YouTube playlist so that you can play the videos in a media player that supports m3u files.')
-st.write('- Create custom playlists by combining videos from multiple YouTube playlists into a single m3u file.')
-st.write('- Share a YouTube playlist with someone else by sending them an m3u file instead of a link to the playlist.')
-
-st.write('## Disclaimer')
-st.write('Please note that `yt2m3u` is an experimental app for testing and development purposes only. Use it at your own risk.')
-st.write('When using `yt2m3u`, please be aware of copyright issues and YouTube policies. It is your responsibility to ensure that you have the necessary rights and permissions to download and use the content from YouTube playlists.')
+if __name__ == '__main__':
+    main()
